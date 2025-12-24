f3696fe83aef4997f564c04912bcf751b94bdfd0
Scalezix Academy <academy@scalezix.com>
2025-12-24 16:31:21 +0530

    Update Footer address to simplified format: iHub Ahmedabad, Gujarat

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-start space-x-3">
                 <MapPin className="h-5 w-5 text-blue-400 mt-1 flex-shrink-0" />
-                <span className="text-gray-300">iHub (IIT Gandhinagar Innovation & Incubation Centre), PDPU Innovation & Incubation Centre, Raisan, Gandhinagar, Gujarat 382007</span>
+                <span className="text-gray-300">iHub Ahmedabad, Gujarat</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Phone className="h-5 w-5 text-blue-400" />
